ea69b30796cd0cb32d867c5e422939d7551fca8c
iamkosgei <kosgeik2930@gmail.com>
2019-06-18 18:11:15 +0300

    activity to load display members belonging to a given group

--- app/src/main/java/me/kosgei/mookh/ui/GroupMembersActivity.java
+++ app/src/main/java/me/kosgei/mookh/ui/GroupMembersActivity.java
@@ -1,25 +1,42 @@
 package me.kosgei.mookh.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.kosgei.mookh.AddMembersFragment;
 import me.kosgei.mookh.R;
+import me.kosgei.mookh.SendMailToAllFragment;
+import me.kosgei.mookh.SendMessageDialogFragment;
+import me.kosgei.mookh.SendSmsToAllFragment;
 import me.kosgei.mookh.ui.adapters.GroupListAdapter;
 import me.kosgei.mookh.ui.adapters.MemberListAdapter;
 import me.kosgei.mookh.ui.model.User;
 
-public class GroupMembersActivity extends AppCompatActivity {
+public class GroupMembersActivity extends AppCompatActivity  implements View.OnClickListener{
     @BindView(R.id.member_list)
     RecyclerView recyclerView;
+
+    @BindView(R.id.add_member)
+    Button addMember;
+
+    @BindView(R.id.email_all)
+    Button emailAll;
+
+    @BindView(R.id.text_all)
+    Button textAll;
 
     private MemberListAdapter memberListAdapter;
     List<User> userList = new ArrayList<>();
@@ -31,6 +48,10 @@ public class GroupMembersActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+
+        addMember.setOnClickListener(this);
+        emailAll.setOnClickListener(this);
+        textAll.setOnClickListener(this);
 
 
         userList.add(new User("john","doe","johndoe@gmail.com","0733332630"));
@@ -47,5 +68,38 @@ public class GroupMembersActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == addMember)
+        {
+            showAddGroupFragment();
+        }
+
+        if (v == emailAll)
+        {
+            FragmentManager fm = this.getSupportFragmentManager();
+            SendMailToAllFragment sendMailToAllFragment = new SendMailToAllFragment();
+            sendMailToAllFragment.show(fm,"");
+
+        }
+        if (v == textAll)
+        {
+            FragmentManager fm = this.getSupportFragmentManager();
+            SendSmsToAllFragment sendSmsToAllFragment = new SendSmsToAllFragment();
+            sendSmsToAllFragment.show(fm,"");
+
+        }
+
+
+    }
+
+    public void showAddGroupFragment()
+    {
+        FragmentManager fm = this.getSupportFragmentManager();
+        AddMembersFragment addMembersFragment = new AddMembersFragment();
+        addMembersFragment.show(fm,"");
+
     }
 }
